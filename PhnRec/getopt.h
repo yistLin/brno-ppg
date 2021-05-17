acf990b2929240e92daf64677448ef2885f86e5f
Yist Lin <yishen992@gmail.com>
2021-05-17 21:01:56 +0800

    make PhnRec compilable on modern archlinux

--- PhnRec/getopt.h
+++ PhnRec/getopt.h
@@ -10,8 +10,9 @@
  *                                                                        *
  **************************************************************************/
 
+extern "C" {
 extern char *optarg;
 extern int optind;
+}
 
 int getopt(int argc, char **argv, char *options);
-
